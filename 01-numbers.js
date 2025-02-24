3dacb5e417e9468ebd2addf6669af2d8cb632867
Veniamin Evstratov <dzimmutenno@gmail.com>
2025-02-24 17:59:40 +0500

    3:544

---
+++ 01-numbers.js
@@ -0,0 +1,35 @@
+/*const num = 42 // integer
+const float = 42.42 // float
+const pow = 10000 
+const big = 1_000_000
+
+console.log(Number.MAX_SAFE_INTEGER)
+console.log(Math.pow(2, 53) -1 )
+console.log(Number.MIN_SAFE_INTEGER)
+console.dir(Number.MAX_VALUE)
+console.dir(Number.MIN_VALUE)
+console.log(Number.POSITIVE_INFINITY)
+console.log(Number.NEGATIVE_INFINITY)
+console.log(1/0)
+console.log(Number.isFinite(Infinity))
+console.log(23 / undefined)
+const weird = 23 / undefined
+console.log(Number.isNaN(weird))
+console.log(Number.isNaN(12)) */
+
+
+const strInt = '42' 
+const strFloat = '42.42'
+
+//console.log(Number(strInt))
+//console.log(Number(strFloat))
+
+/*console.log(parseInt(strInt))
+console.log(Number.parseInt(strFloat))
+console.log(Number.parseFloat(strFloat))*/
+
+console.log(+strInt, +strFloat)
+
+console.log(0.1 + 0.2)
+
+3:54
