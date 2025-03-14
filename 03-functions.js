462106873bf6b8e0fa14257e71ee5040fd039c40
Veniamin Evstratov <dzimmutenno@gmail.com>
2025-03-14 19:58:05 +0500

    4.34

--- 03-functions.js
+++ 03-functions.js
@@ -1,1 +1,22 @@
-4:24
+// Function Declaration 
+function greet(name) {
+    console.log('Hello - ', name)
+}
+// Function Expression можно использовать 
+//  функцию только после объявления
+const greet2 = function (name) {
+    console.log('2 Hello - ', name)
+}
+
+greet('Vladilen')
+greet2('Vladilen')
+
+//console.dir(greet.toString())
+
+/*setTimeout(function() {
+     greet('Vladilen2')
+}, 1500)*/
+
+/*setInterval(function () {
+    console.log(1)
+}, 2000)*/
