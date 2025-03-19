ade656ca02b8adb64ff9f8a11a011bb0e0207efc
Veniamin Evstratov <dzimmutenno@gmail.com>
2025-03-19 19:29:23 +0500

    5.0

--- 04-date.js
+++ 04-date.js
@@ -1,1 +1,15 @@
-const now = console.log(new Date())
+const now = new Date()
+
+const start = new Date(1000 * 60 * 60 * 24 * 
+365)
+/*
+const date = new Date(2011, 0, 4, 12, 42, 12)
+
+console.log(now.getFullYear())
+
+now.setFullYear(2055)*/
+
+console.log(now.toDateString())
+console.log(now.toTimeString())
+console.log(now.toLocaleDateString())
+console.log(now.toLocaleDateString())
