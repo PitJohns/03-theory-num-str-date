6d3cf051e021f8a5c9f69ae00c70d75e9da131b9
Veniamin Evstratov <dzimmutenno@gmail.com>
2025-03-24 20:04:44 +0500

    5.13

--- 04-date.js
+++ 04-date.js
@@ -1,4 +1,4 @@
-const now = new Date()
+/*const now = new Date()
 
 const start = new Date(1000 * 60 * 60 * 24 * 
 365)
@@ -9,7 +9,54 @@ console.log(now.getFullYear())
 
 now.setFullYear(2055)*/
 
-console.log(now.toDateString())
+/*console.log(now.toDateString())
 console.log(now.toTimeString())
 console.log(now.toLocaleDateString())
-console.log(now.toLocaleDateString())
+console.log(now.toLocaleTimeString())
+*/
+// ==========
+
+let mode = 'time'
+const output = document.getElementById('output')
+const fullBtn = document.getElementById('full')
+const dateBtn = document.getElementById('date')
+const timeBtn = document.getElementById('time')
+
+function bindMode(name) {
+  return function() {
+    mode = name
+    update()
+  }
+}
+
+fullBtn.onclick = bindMode('full')
+
+dateBtn.onclick = bindMode('date')
+
+timeBtn.onclick = bindMode('time')
+
+
+update()
+
+setInterval(update, 10)
+
+function update() {
+  output.textContent = format(mode)
+}
+
+// Pure Function
+function format(formatMode) {
+  const now = new Date()
+  switch (formatMode) {
+    case 'time':
+       return now.toLocaleTimeString() + '.' +
+       now.getMilliseconds()
+    case 'date':
+       return now.toLocaleDateString() 
+    case 'full':
+       return now.toLocaleDateString() + ' ' +
+       now.toLocaleTimeString()
+    default:
+       return now.toLocaleTimeString
+  }
+}
